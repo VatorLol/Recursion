2c0016bbd90544f2acf3b1ac11b8597fc08a0362
vatorlol <eyub2001@gmail.com>
2018-09-09 20:56:26 +0300

    fibonacci update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(factoriel(0));
+        System.out.println(fibonacci(10));
     }
 
+    /*
+    * @param n >= 0
+    * @return the factoriel of n
+    * */
     private static int factoriel(int n){
-
         if (n == 1 || n == 0)
             return 1;
         else
             return factoriel(n - 1) * n;
+    }
 
+    /*
+    * @param n >= 0
+    * @return the nth fibonacci number
+    * */
+    private static int fibonacci(int n){
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
